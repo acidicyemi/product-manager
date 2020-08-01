f093dfa6f5282cbbf4216a646c09a434c7cdea8b
Ajayi Adeyemi <ajayiadeyemi@gmail.com>
2020-08-01 02:23:19 +0100

    product crud added

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,5 @@
-var Product = require("../models").Product
+var models = require("../models")
+const Product = models.Product
 Promise   = require('bluebird'),
 
 module.exports = {
@@ -12,4 +13,52 @@ module.exports = {
             }
           })();
     },
+
+    createProducts: function (req, res, next) {
+      Promise.coroutine(function*() {
+          try {
+            var product = yield Product.create(req.body);
+            return res.status(200).send({success: true, product});
+          } catch (err) {
+            return res.status(200).send({success: false, error: (err)});
+          }
+        })();
+      },
+
+      viewProduct: function (req, res, next) {
+        Promise.coroutine(function*() {
+          try {
+            var product = yield Product.findOne({where: {id: req.params.id}});
+            return res.status(200).send({success: true, product});
+          } catch (err) {
+            return res.status(200).send({success: false, error: (err)});
+          }
+      })();
+    },
+
+    updateProduct: function (req, res) {
+      Promise.coroutine(function*() {
+        try {
+          var updatedProduct = yield Product.update(req.body, {where: {id: req.params.id}});
+          console.log(("test",updatedProduct[0]));
+          return res.status(200).send({success: true, product });
+        } catch (err) {
+          return res.status(200).send({success: false, error: (err)});
+        }
+      })();
+    }, 
+
+    deleteProduct: function (req, res) {
+      Promise.coroutine(function*() {
+        try {
+          var deletedProduct = yield Product.destroy({where: {id: req.params.id}});
+          if(!deletedProduct) {
+            return res.status(200).send({success: false, message: 'Invalid Product id'});
+          }
+          return res.status(200).send({success: true, message: 'success'});
+        } catch (err) {
+          return res.status(200).send({success: false, error: (err)});
+        }
+      })();
+    }
 }
--- routes/product.js
+++ routes/product.js
@@ -4,5 +4,9 @@ var router = express.Router();
 let pc = require("../controllers/product")
 /* GET users listing. */
 router.get('/', pc.listProducts);
+router.post("/", pc.createProducts)
+router.get("/view/:id", pc.viewProduct)
+router.put("/update/:id", pc.updateProduct)
+router.delete("/:id", pc.deleteProduct)
 
 module.exports = router;
